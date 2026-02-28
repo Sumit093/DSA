1a1ea7c8537c1bc967a6bdd96265110fcc81c28d
Sumit Haral <146067244+Sumit093@users.noreply.github.com>
2026-02-28 12:25:41 +0530

    Time: 19 ms (22.06%) | Memory: 45.9 MB (13.14%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -4,11 +4,7 @@ class Solution {
         int n = nums.length;
         int ans = nums[0] + nums[1] + nums[2];
         int i=0;    
-        while(i<n-2){
-            // if(i > 0 && nums[i] == nums[i - 1]){
-            //     i++;
-            //     continue;
-            // }   
+        while(i<n-2){ 
             
             int left = i+1;
             int right = n-1;
