37ff48a83afdfde9f903a3a17b9a99cfa78795a1
Sumit Haral <146067244+Sumit093@users.noreply.github.com>
2026-03-15 19:08:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (86.87%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.java
@@ -1,52 +1,18 @@
 class Solution {
     public List<Integer> targetIndices(int[] nums, int target) {
-        Arrays.sort(nums);
         List<Integer> ans = new ArrayList<>();
-        
-        
-        int f = lower(nums,target);
-        if(f==nums.length||nums[f]!=target){
+        int small=0,equal=0,n=nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]<target){
+                small++;
+            }
+            else if(nums[i]==target){
+                equal++;
+            }
+        }
+        for(int i=0;i<equal;i++){
+            ans.add(i+small);
+        }
         return ans;
-        }
-        int l = upper(nums,target);
-
-        for(int i=f;i<=l;i++){
-            ans.add(i);
-        }
-        return ans;
-        
-    }
-    public int lower(int[] nums, int target){
-        int n = nums.length;
-        int low = 0;
-        int high = n-1;
-        int mid =low+(high-low)/2;
-        while(low<=high){
-            mid = low+(high-low)/2;
-            if(nums[mid]>=target){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
-            }
-        }
-        return low;
-    }
-
-    public int upper(int[] nums, int target){
-        int n = nums.length;
-        int low = 0;
-        int high = n-1;
-        int mid =low+(high-low)/2;
-        while(low<=high){
-            mid = low+(high-low)/2;
-            if(nums[mid]>target){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
-            }
-        }
-        return high;
     }
 }
