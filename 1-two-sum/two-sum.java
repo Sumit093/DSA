e3197bec90118dd2b51eee5d5aab229d4ce022dd
Sumit Haral <146067244+Sumit093@users.noreply.github.com>
2026-07-14 23:05:46 +0530

    Time: 1 ms (99.37%) | Memory: 47.1 MB (58.28%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,20 +1,61 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target){
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++){
-                if (nums[i] + nums[j] == target){
-                    return new int[] {i, j};
-                }
-            }
-        }
-        return new int[] {}; 
+    int size;
+    int[] keys;
+    int[] values;
+
+    void init(int sz) {
+        size = sz;
+        keys = new int[size];
+        values = new int[size];
     }
 
-    public void main(String[] args){
-        int[] arr = {2, 7, 11, 15};
-        int target = 9;
+    public int hash(int x) {
+        x = ((x >> 16) ^ x) * 0x45d9f3b;
+        x = ((x >> 16) ^ x) * 0x45d9f3b;
+        x = (x >> 16) ^ x;
+        return Math.floorMod(x, size);
+    }
 
-        int[] result = twoSum(arr, target);
-        System.out.println(Arrays.toString(result));
+    void set(int key, int value) {
+        int code = hash(key);
+        int k = keys[code];
+        int v = values[code];
+        while (v != 0 && key != k) {
+            code = (code + 1) % size;
+            k = keys[code];
+            v = values[code];
+        }
+        keys[code] = key;
+        values[code] = value + 1;
+    }
+
+    int get(int key) {
+        int code = hash(key);
+        int k = keys[code];
+        int v = values[code];
+        while (v != 0 && key != k) {
+            code = (code + 1) % size;
+            k = keys[code];
+            v = values[code];
+        }
+        return v - 1;
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        init(nums.length * 2 + 1);
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; ++i) {
+            int needed = target - nums[i];
+            int got = get(needed);
+            if (got != -1) {
+                result[0] = i;
+                result[1] = got;
+                break;
+            }
+            set(nums[i], i);
+        }
+
+        return result;
     }
 }
